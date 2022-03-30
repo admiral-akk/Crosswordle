c28523a9eef811474402e96ddc42ac642d857dc9
Kuba Karpierz <admiral-akk@users.noreply.github.com>
2022-03-30 14:44:23 -0400

    Adjusted size of elements

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -48,7 +48,8 @@ public class BoardManager : MonoBehaviour
         ResetBoard();
     }
 
-    private Vector3 Spacing => new Vector3(1 + WordSpacing, 1 + GuessSpacing);
+
+    private Vector3 Spacing => new Vector3(0.8f + WordSpacing, 0.8f + GuessSpacing);
     private Vector3 Offset => new Vector3((1f - WordLength) / 2, (GuessLimit - 1f) / 2 + YOffset);
 
     private Vector3 Position(int letterIndex, int guessIndex)
